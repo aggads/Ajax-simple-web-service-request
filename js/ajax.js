eb63a4bbccfb1b2374a6967f0be1e12400542f51
aggads <aggad.samy@gmail.com>
2018-11-12 14:22:48 +0100

    hidde return on home page

--- js/ajax.js
+++ js/ajax.js
@@ -3,6 +3,19 @@
 
 function loadDoc(){
 
+var showReturn = document.querySelector('.return');
+// function show() {
+    // var test = showReturn.innerHTML = presentation;
+
+    // showReturn.classList.add("elementToAnimate");
+
+    showReturn.style.visibility = "visible";
+
+    // return test;
+
+// }
+
+
 //Create the XHR Object
 var xhr = new XMLHttpRequest;
 //Call the open function, GET-type of request, url, true-asynchronous
@@ -40,57 +53,3 @@ xhr.onerror = function(){
 // , 5000);
 
 
-// let xhr = new XMLHttpRequest; 
-// xhr.open('GET', 'https://thatsthespir.it/api', true); 
-// xhr.onload = function() { 
-// 	if(this.status === 200) { 
-// 		let test = JSON.parse(this.responseText); 
-// 		let someThing = document.querySelector('#quote'); 
-// 		someThing.innerHTML = 
-// 		<ul> 
-// 			<li>Quote: ${test.quote}</li> 
-// 			<li> Author:${test.author}</li> 
-// 		</ul> 
-// 	} } 
-// xhr.onerror = function(){ 
-// 	let someThing = document.querySelector('#quote'); 
-// 	someThing.innerHTML = "Error stupid get off" } xhr.send();
-
-
-// function myFunction(xml) {
-//   var i;
-//   var xmlDoc = xml.responseXML;
-//   var table="<tr><th>Title</th><th>Artist</th></tr>";
-//   var x = xmlDoc.getElementsByTagName("CD");
-//   for (i = 0; i <x.length; i++) { 
-//     table += "<tr><td>" +
-//     x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-//     "</td><td>" +
-//     x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-//     "</td></tr>";
-//   }
-//   document.getElementById("demo").innerHTML = table;
-// }
-
-
-//Common Types of HTTP Statuses
-// 200: OK
-// 404: ERROR
-// 403: FORBIDDEN
-
-//Call the fetch fonction
-// fetch('https://thatsthespir.it/api')
-// .then(res => res.json()) //response type
-// .then(data => console.log(data)); //log the data
-
-// async function getData() {
-	
-// 	var response = await fetch('https://thatsthespir.it/api');
-
-// 	var data = await response.json()
-
-// 	return data;
-// }
-
-// getData()
-// .then(data => console.log(data));
